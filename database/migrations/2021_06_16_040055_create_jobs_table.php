684add3cc3c36218154f04007a7591173ce2b315
Conor West <westcl4@student.op.ac.nz>
2021-06-16 16:27:07 +1200

    Added models migrations for jobs filaments

---
+++ database/migrations/2021_06_16_040055_create_jobs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('customer_id')->references('id')->on('customers');
+            $table->integer('fil_id')->references('id')->on('filaments');
+            $table->integer('fil_length');
+            $table->integer('fil_volume');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
