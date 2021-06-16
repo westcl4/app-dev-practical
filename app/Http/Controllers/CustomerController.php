d7572e13a61f5f9d73d187f3168a2f3653b8ec5b
Conor West <westcl4@student.op.ac.nz>
2021-06-16 18:55:24 +1200

    Added customer controller

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,8 +3,65 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Customer;
+use Validator;
 
 class CustomerController extends Controller
 {
-    //
+    public function createCustomer(Request $request){
+        // $validator = Validator::make($request->all(), [
+        //     'first_name' => 'required',
+        //     'last_name' => 'required',
+        //     'phone' => 'required',
+        //     'email' => 'required'
+        // ]);
+    
+        // if ($validator->fails()) {
+        //     return response()->json(['message' => $validator->errors()], 403);
+        // } else {
+            Customer::create($request->all());
+            return response()->json(['message' => 'Customer created.'], 201);
+        // }
+    
+    }
+    public function updateCustomer(Request $request, $id){
+        $customers = Customer::query();
+        if ($customers->where('id', $id)->exists()) {
+            $customer = $customers->find($id);
+            $customer->first_name = is_null($request->first_name) ? $customer->first_name : $request->first_name;
+            $customer->last_name = is_null($request->last_name) ? $customer->last_name : $request->last_name;
+            $customer->phone = is_null($request->phone) ? $customer->phone : $request->phone;
+            $customer->email = is_null($request->email) ? $customer->email : $request->email;
+            $customer->save();
+            return response()->json(['message' => 'Customer updated.'], 200);
+        } else {
+            return response()->json(['message' => 'Customer not found.'], 404);
+        }
+    }
+    public function deleteCustomer($id){
+        $customers = Customer::query();
+        if ($customers->where('id', $id)->exists()) {
+            $customer = $customers->find($id);
+            $customer->delete();
+            return response()->json(['message' => 'Customer deleted.'], 202);
+        } else {
+            return response()->json(['message' => 'Customer not found.'], 404);
+        }
+    }
+    public function getAllCustomers(Request $request){
+        $customers = Customer::query();
+        if ($request->get('first_name')) {
+            $customers->where('first_name', '=', $request->get('first_name'));
+        }
+        return $customers->get();
+    }
+    public function getCustomer($id){
+        $customers = Customer::query();
+        if ($customers->where('id', $id)->exists()) {
+            $customer = $customers->where('id', $id)->get();
+            return response($customer, 200);
+        } else {
+            return response()->json(['message' => 'Customer not found.'], 404);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,24 +2,27 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-// Route::group(['prefix' => 'customers'], function() {
-//     Route::get('/customers', function ($id) {
-//         Route::post('/customers','PracticalController@createCustomer');
-//         Route::put('/{id}', 'PracticalController'
-//     });
-// });
+
+Route::group(['prefix' => 'customers'], function() {
+        Route::post('/','CustomerController@createCustomer');
+        Route::put('/{id}', 'CustomerController@updateCustomer');
+        Route::delete('/{id}', 'CustomerController@deleteCustomer');
+        Route::get('/', 'CustomerController@getAllCustomers');
+        Route::get('/{id}', 'CustomerController@getCustomers');
+});
+
+Route::group(['prefix' => 'jobs'], function() {
+    Route::get('/jobs', function ($id) {
+        Route::post('/jobs','JobController@createJob');
+        Route::put('/{id}', 'JobController@updateJob');
+        Route::delete('/{id}', 'JobController@deleteJob');
+        Route::get('/', 'JobController@getAllJobs');
+        Route::get('/{id}', 'JobController@getJob');
+    });
+});
